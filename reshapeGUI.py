f3c3aea70f6c2d7c677301b5ce9e391259931b6d
Nawaf <40125781+Silvakabe@users.noreply.github.com>
2025-03-07 03:57:43 +0300

    #minor auto-convert and fix no question mark

--- reshapeGUI.py
+++ reshapeGUI.py
@@ -1,18 +1,18 @@
-from tkinter import font, Tk, Label, Text, Button, Checkbutton, IntVar, END, Frame, LEFT
+from tkinter import font, Tk, Label, Text, Button, Frame, LEFT, END, IntVar, Checkbutton
 from arabic_reshaper import ArabicReshaper
 
 configuration = {
     'delete_harakat': False
 }
 root = Tk()
-root.geometry("400x500")
+root.geometry("400x400")
 root.title("umi ara_resh test")
 
 reshaper = ArabicReshaper(configuration=configuration)
 
-def Take_input():
-    INPUT = inputtxt.get("1.0",'end-1c')
-    Output.delete(0.0,END)
+def Take_input(event=None):  # Event argument needed for key binding
+    INPUT = inputtxt.get("1.0", 'end-1c')
+    Output.delete("1.0", END)
     reshape = reshaper.reshape(INPUT)
     if check_var.get() == 0:
         Output.insert(END, reshape)
@@ -29,27 +29,30 @@ def copy_output():
 def paste_text():
     inputtxt.delete("1.0", END)
     inputtxt.insert(END, root.clipboard_get())
+    Take_input()
 
-# استخدام خط يدعم الحركات
-arabic_font = font.Font(family="Traditional Arabic", size=18)
+
+arabic_font = font.Font(family="Arial", size=18)
 
 l = Label(root, text="Enter text")
 inputtxt = Text(root, height=5, width=40, bg="light yellow", font=arabic_font)
 Output = Text(root, height=5, width=40, bg="light cyan", font=arabic_font)
 
+
+inputtxt.bind("<KeyRelease>", Take_input)
+
 button_frame = Frame(root)
 paste_button = Button(button_frame, text="Paste", command=paste_text)
-Display = Button(button_frame, text="Convert", command=Take_input)
 copy_output_button = Button(button_frame, text="Copy Output", command=copy_output)
 
 check_var = IntVar()
 check = Checkbutton(root, text="Include {a:r:}", variable=check_var)
+check_var.trace_add("write", lambda *args: Take_input())  # Update output when check_var changes
 
 l.pack()
 inputtxt.pack()
 button_frame.pack()
 paste_button.pack(side=LEFT)
-Display.pack(side=LEFT)
 copy_output_button.pack(side=LEFT)
 Output.pack()
 check.pack()
